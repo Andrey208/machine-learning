d0fcd6030e2890d125711d9e2d4e9a9bc370d054
Andrey208 <jukandrey208@gmail.com>
2020-02-12 22:37:02 +0300

    Init console project

---
+++ ML.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ML.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
