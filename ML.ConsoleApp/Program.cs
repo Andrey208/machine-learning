325ec5b9ff9e9a1562f693c56901a5a8574e58e0
Andrey208 <jukandrey208@gmail.com>
2020-02-12 22:56:31 +0300

    Create test sentiment analisis

--- ML.ConsoleApp/Program.cs
+++ ML.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MlML.Model;
 
 namespace ML.ConsoleApp
 {
@@ -6,7 +7,16 @@ namespace ML.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = new ModelInput
+            {
+                SentimentText = "Are you nuts?"
+            };
+            var result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}{Environment.NewLine}Is Toxic: {result.Prediction}");
+
+            input.SentimentText = "Good job";
+            result = ConsumeModel.Predict(input);
+            Console.WriteLine($"Text: {input.SentimentText}{Environment.NewLine}Is Toxic: {result.Prediction}");
         }
     }
 }
